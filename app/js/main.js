fa8b24deeccc2c15ffc3af14fc1e64cf4fdeb127
shini322 <33940422+shini322@users.noreply.github.com>
2020-09-24 20:58:35 +0300

    Services

--- app/js/main.js
+++ app/js/main.js
@@ -1,5 +1,10 @@
 // $('.main-slider .swiper-container').each(function(){
 //     var main_slider = new Swiper(this, {
+
+// const { default: Swiper } = require("swiper");
+
+// const { default: Swiper } = require("swiper");
+
      
 //         navigation: {
 //             nextEl: $(this).parent().find('.swiper-button-next'),
@@ -23,3 +28,74 @@ var aboutSlider = new Swiper('.main-slider .swiper-container', {
         prevEl: '.main-slider .swiper-button-prev',
       },
   });
+
+$('.content-slider').each(function(){
+  if($(this).hasClass('price__slider') || $(this).hasClass('questions__slider')){
+    var contentSlider = new Swiper(this, {    
+      spaceBetween: 60,
+      slidesPerView: 1,
+      navigation: {
+        nextEl: $(this).prev().find('.content-slider__arrow-right')[0],
+        prevEl: $(this).prev().find('.content-slider__arrow-left')[0],
+      },
+    });
+  } else if($(this).hasClass('reviews__slider')){
+    var contentSlider = new Swiper(this, {    
+      spaceBetween: 30,
+      slidesPerView: 3,
+      navigation: {
+        nextEl: $(this).prev().find('.content-slider__arrow-right')[0],
+        prevEl: $(this).prev().find('.content-slider__arrow-left')[0],
+      },
+    });
+  } else if ($(this).hasClass('gallery__slider')){
+    var contentSlider = new Swiper(this, {    
+      spaceBetween: 30,
+      slidesPerView: 3,
+      navigation: {
+        nextEl: $(this).prev().find('.content-slider__arrow-right')[0],
+        prevEl: $(this).prev().find('.content-slider__arrow-left')[0],
+      },
+    });
+  } else if($(this).hasClass('articles__slider')){
+    var contentSlider = new Swiper(this, {    
+      spaceBetween: 10,
+      slidesPerView: 4,
+      navigation: {
+        nextEl: $(this).prev().find('.content-slider__arrow-right')[0],
+        prevEl: $(this).prev().find('.content-slider__arrow-left')[0],
+      },
+    });
+  } else {
+    var contentSlider = new Swiper(this, {    
+      spaceBetween: 60,
+      slidesPerView: 2,
+      navigation: {
+        nextEl: $(this).prev().find('.content-slider__arrow-right')[0],
+        prevEl: $(this).prev().find('.content-slider__arrow-left')[0],
+      },
+    });
+  }
+  
+});
+
+//Table tips
+
+$('.price__icon-container').mouseover(function(){
+  $(this).find('.price__tip').addClass('active');
+});
+$('.price__icon-container').mouseleave(function(){
+  $(this).find('.price__tip').removeClass('active');
+});
+
+
+//FAQ dropdown
+
+
+$('.questions-dropdown__title-block').click(function(){
+  $(this).toggleClass('active');
+  $(this).next().toggleClass('active').slideToggle();
+  $(this).find('.questions-dropdown__arrow-icon').toggleClass('active');
+});
+
+
