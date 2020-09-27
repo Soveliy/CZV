336c03ec5b92ef8398e031e6fb934aead8a63c95
Sovel <nikita.elagin2012@yandex.ru>
2020-09-27 16:18:20 +0300

    urpade

--- app/js/main.js
+++ app/js/main.js
@@ -29,6 +29,23 @@ var aboutSlider = new Swiper('.main-slider .swiper-container', {
       },
   });
 
+// const sliders = document.querySelectorAll('.swiper-container');
+
+// sliders.forEach((el) => {
+// 	let swiper = new Swiper(el, {
+// 		slidesPerView: 3,
+// 		spaceBetween: 10,
+// 		loop: true,
+// 		pagination: {
+// 			el: el.querySelector('.swiper-pagination'),
+// 			clickable: true,
+// 		},
+// 		navigation: {
+// 			nextEl: el.querySelector('.swiper-button-next'),
+// 			prevEl: el.querySelector('.swiper-button-prev'),
+// 		},
+// 	});
+// });
 $('.content-slider').each(function(){
   if($(this).hasClass('price__slider') || $(this).hasClass('questions__slider')){
     var contentSlider = new Swiper(this, {    
@@ -98,4 +115,47 @@ $('.questions-dropdown__title-block').click(function(){
   $(this).find('.questions-dropdown__arrow-icon').toggleClass('active');
 });
 
+$('.options-buttons__search-btn').click(function(){
+    $(this).next().toggleClass('js-active');
+  });
+  
 
+
+
+// upload width Browser
+windowSize = $(window).width(); 
+$(window).on('resize', function(event){
+windowSize = $(window).width(); 
+});
+
+
+$(".header__buttons-item-icon--edit").on('click', function(){
+    $(".mobile-buttons").slideToggle();
+    $(".mobile-menu").hide();
+    $(".burger").removeClass("js-active")
+})
+$(".burger").on('click', function(){
+    $(this).toggleClass("js-active")
+    $(".mobile-menu").slideToggle();
+    $(".mobile-buttons").hide();
+})
+$(".mobile-menu__nav > ul > li > span").on('click', function(){
+
+  
+    if(windowSize < 1025){
+    if ($(this).hasClass("js-active")) {
+        $(this).removeClass("js-active");
+        $(this).next().removeClass("js-active");
+        $(this).next().slideToggle();
+        
+    } else {
+      
+      $(this).closest(".mobile-menu__nav").find("ul.js-active").slideToggle();
+      $(this).closest(".mobile-menu__nav").find("ul.js-active").removeClass("js-active");
+      $(".mobile-menu__nav > ul > li > span").removeClass("js-active");
+      $(this).addClass("js-active");
+      $(this).next().addClass("js-active");
+      $(this).next().slideToggle();
+    }
+    }  
+  });
